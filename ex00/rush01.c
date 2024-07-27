2403d81fb6e05a338055002e9d434a66c94dda16
RANIALSV1 <raniahaddi6@gmail.com>
2024-07-27 20:11:23 +0100

    rush 00

---
+++ ex00/rush01.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush01.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rahaddi <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/27 19:53:06 by rahaddi           #+#    #+#             */
+/*   Updated: 2024/07/27 19:53:36 by rahaddi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+void	rush_print(int i, int j, int columns, int lines)
+{
+	if (i == 0 && j == 0)
+		ft_putchar('/');
+	else if (i == columns - 1 && j == 0)
+		ft_putchar('\\');
+	else if (i == 0 && j == lines - 1)
+		ft_putchar('\\');
+	else if (i == columns - 1 && j == lines - 1)
+		ft_putchar('/');
+	else if (j == 0 || j == lines - 1)
+		ft_putchar('*');
+	else if (i == 0 || i == columns - 1)
+		ft_putchar('*');
+	else
+		ft_putchar(' ');
+}
+
+void	rush(int columns, int lines)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	if (columns <= 0 || lines <= 0)
+		write(1, "error", 6);
+	while (j < lines)
+	{
+		i = 0;
+		while (i < columns)
+		{
+			rush_print(i, j, columns, lines);
+			i++;
+		}
+		j++;
+		ft_putchar('\n');
+	}
+}
---
+++ ex00/rush04.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush04.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rahaddi <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/27 19:52:28 by rahaddi           #+#    #+#             */
+/*   Updated: 2024/07/27 19:52:29 by rahaddi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+void	rush_print(int i, int j, int columns, int lines)
+{
+	if (i == 0 && j == 0)
+		ft_putchar('A');
+	else if (i == 0 && j == lines - 1)
+		ft_putchar('C');
+	else if (i == columns - 1 && j == 0)
+		ft_putchar('C');
+	else if (i == columns - 1 && j == lines - 1)
+		ft_putchar('A');
+	else if (j == 0 || j == lines - 1)
+		ft_putchar('B');
+	else if ((i == 0 || i == columns - 1))
+		ft_putchar('B');
+	else
+		ft_putchar(' ');
+}
+
+void	rush(int columns, int lines)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	if (columns <= 0 || lines <= 0)
+		write(1, "error", 6);
+	while (j < lines)
+	{
+		i = 0;
+		while (i < columns)
+		{
+			rush_print(i, j, columns, lines);
+			i++;
+		}
+		j++;
+		ft_putchar('\n');
+	}
+}
